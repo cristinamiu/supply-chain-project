710fdbd1e5d1a61ecdf1c9c680f4e0f80a8cf509
Cristina <cristinamaria.miulescu@gmail.com>
2022-05-21 19:52:03 +0300

    Initial commit

---
+++ contracts/Item.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./ItemManager.sol";
+
+contract Item {
+    uint public priceInWei;
+    uint public index;
+    uint public pricePaid;
+
+    ItemManager parentContract;
+
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(pricePaid == 0, "Item is paid already");
+        require(priceInWei == msg.value, "Only full payments accepted");
+        pricePaid += msg.value;
+
+        (bool success, ) = address(parentContract).call{value:msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "The transaction wasn' successful, canceling");
+    }
+}
